6ff8cfe93d21aad3566b72b6d64fb4213b38cbb2
theb0b12 <103223932+theb0b12@users.noreply.github.com>
2026-01-22 10:51:31 -0700

    changed some stuff around idk

---
+++ node_modules/ergogen/src/io.js
@@ -0,0 +1,118 @@
+const yaml = require('js-yaml')
+const jszip = require('jszip')
+const makerjs = require('makerjs')
+
+const u = require('./utils')
+const a = require('./assert')
+const kle = require('./kle')
+
+const package_json = require('../package.json')
+
+const fake_require = exports.fake_require = injection => name => {
+    const dependencies = {
+        makerjs
+    }
+    if (name.endsWith('package.json')) {
+        return package_json
+    } else if (dependencies[name]) {
+        return dependencies[name]
+    } else throw new Error(`Unknown dependency "${name}" among the requirements of injection "${injection}"!`)
+}
+
+exports.unpack = async (zip) => {
+
+    // main config text (has to be called "config.ext" where ext is one of yml/yaml/json/js)
+    const candidates = zip.file(/^config\.(ya?ml|json|js)$/)
+    if (candidates.length != 1) {
+        throw new Error('Ambiguous config in bundle!')
+    }
+    const config_text = await candidates[0].async('string')
+    const injections = []
+
+    // bundled footprints
+    const fps = zip.folder('footprints')
+    const module_prefix = 'const module = {};\n\n'
+    const module_suffix = '\n\nreturn module.exports;'
+    for (const fp of fps.file(/.*\.js$/)) {
+        const name = fp.name.slice('footprints/'.length).split('.')[0]
+        const text = await fp.async('string')
+        const parsed = new Function('require', module_prefix + text + module_suffix)(fake_require(name))
+        // TODO: some sort of footprint validation?
+        injections.push(['footprint', name, parsed])
+    }
+
+    // bundled pcb templates
+    const tpls = zip.folder('templates')
+    for (const tpl of tpls.file(/.*\.js$/)) {
+        const name = tpl.name.slice('templates/'.length).split('.')[0]
+        const text = await tpl.async('string')
+        const parsed = new Function('require', module_prefix + text + module_suffix)(fake_require(name))
+        // TODO: some sort of template validation?
+        injections.push(['template', name, parsed])
+    }
+
+    return [config_text, injections]
+}
+
+exports.interpret = (raw, logger) => {
+    let config = raw
+    let format = 'OBJ'
+    if (a.type(raw)() == 'string') {
+        try {
+            config = yaml.load(raw)
+            format = 'YAML'
+        } catch (yamlex) {
+            try {
+                config = new Function(raw)()
+                a.assert(
+                    a.type(config)() == 'object',
+                    'Input JS Code doesn\'t resolve into an object!'
+                )
+                format = 'JS'
+            } catch (codeex) {
+                logger('YAML exception:', yamlex)
+                logger('Code exception:', codeex)
+                throw new Error('Input is not valid YAML, JSON, or JS Code!')
+            }
+        }
+    }
+    
+    try {
+        // assume it's KLE and try to convert it
+        config = kle.convert(config, logger)
+        format = 'KLE'
+    } catch (kleex) {
+        // nope... nevermind
+    }
+
+    if (a.type(config)() != 'object') {
+        throw new Error('Input doesn\'t resolve into an object!')
+    }
+
+    if (!Object.keys(config).length) {
+        throw new Error('Input appears to be empty!')
+    }
+
+    return [config, format]
+}
+
+exports.twodee = (model, options) => {
+    const {debug, svg} = options
+    const assembly = makerjs.model.originate({
+        models: {
+            export: u.deepcopy(model)
+        },
+        units: 'mm'
+    })
+
+    const result = {
+        dxf: makerjs.exporter.toDXF(assembly),
+    }
+    if (debug) {
+        result.yaml = assembly
+    }
+    if (debug || svg) {
+      result.svg = makerjs.exporter.toSVG(assembly)
+    }
+    return result
+}
---
+++ node_modules/ergogen/test/helpers/mock.js
@@ -0,0 +1,175 @@
+const FIXED_TIMESTAMP = 1760558400000; // 2025-10-16T12:00:00.000Z
+
+const OriginalDate = global.Date;
+
+// Mock Date to return a fixed date for deterministic test output
+global.Date = class extends OriginalDate {
+  constructor(...args) {
+    // If the constructor is called with arguments, pass them to the original Date constructor
+    if (args.length > 0) {
+      super(...args);
+    } else {
+      // Otherwise, create a date with the fixed timestamp
+      super(FIXED_TIMESTAMP);
+    }
+  }
+
+  static now() {
+    return FIXED_TIMESTAMP;
+  }
+};
+
+// Copy static properties from the original Date to the mock
+Object.getOwnPropertyNames(OriginalDate)
+  .filter(prop => prop !== 'length' && prop !== 'name' && prop !== 'prototype' && prop !== 'now')
+  .forEach(prop => (global.Date[prop] = OriginalDate[prop]));
+
+exports.inject = (ergogen) => {
+    ergogen.inject('footprint', 'trace_test', {
+        params: {
+            designator: 'T',
+            side: 'F',
+            width: 0.25,
+            P1: {type: 'net', value: 'P1'}
+        },
+        body: p => {
+            return `
+
+                (module trace_test (layer ${p.side}.Cu) (tedit 5CF31DEF)
+
+                    ${p.at /* parametric position */}
+
+                    (pad 1 smd rect (at ${p.isxy(0, 0)} ${p.r}) (size 1 1) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask)
+                        ${p.P1} (solder_mask_margin 0.2))
+
+                    (pad 2 smd rect (at ${p.isxy(5, 5)} ${p.r}) (size 1 1) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask)
+                        ${p.P1} (solder_mask_margin 0.2))
+
+                )
+
+                (segment (start ${p.esxy(0, 0)}) (end ${p.esxy(5, 5)}) (width ${p.width}) (layer ${p.side}.Cu) (net ${p.P1.index}))
+
+            `
+        }
+    })
+
+    ergogen.inject('footprint', 'zone_test', {
+        params: {
+            designator: 'T',
+            side: 'F',
+            P1: {type: 'net', value: 'P1'}
+        },
+        body: p => {
+            return `
+
+                (module zone_test (layer ${p.side}.Cu) (tedit 5CF31DEF)
+
+                    ${p.at /* parametric position */}
+
+                    (pad 1 smd rect (at 0 0 ${p.r}) (size 1 1) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask)
+                        ${p.P1} (solder_mask_margin 0.2))
+
+                    (pad 2 smd rect (at ${p.iaxy(5, 5)} ${p.r}) (size 1 1) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask)
+                        ${p.P1} (solder_mask_margin 0.2))
+
+                )
+
+                (zone (net ${p.P1.index}) (net_name ${p.P1.name}) (layer ${p.side}.Cu) (tstamp 0) (hatch full 0.508)
+                    (connect_pads (clearance 0.508))
+                    (min_thickness 0.254)
+                    (fill yes (arc_segments 32) (thermal_gap 0.508) (thermal_bridge_width 0.508))
+                    (polygon (pts (xy ${p.eaxy(5, 5)}) (xy ${p.eaxy(5, -5)}) (xy ${p.eaxy(-5, -5)}) (xy ${p.eaxy(-5, 5)})))
+                )
+
+            `
+        }
+    })
+
+    ergogen.inject('footprint', 'dynamic_net_test', {
+        params: {
+            designator: 'T',
+            side: 'F'
+        },
+        body: p => {
+            return ` 
+
+                (module dynamic_net_test (layer ${p.side}.Cu) (tedit 5CF31DEF)
+
+                    ${p.at /* parametric position */}
+
+                    (pad 1 smd rect (at 0 0 ${p.r}) (size 1 1) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask)
+                        ${p.local_net('1')} (solder_mask_margin 0.2))
+
+                    (pad 1 smd rect (at 0 0 ${p.r}) (size 1 1) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask)
+                        ${p.local_net('2')} (solder_mask_margin 0.2))
+
+                    (pad 1 smd rect (at 0 0 ${p.r}) (size 1 1) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask)
+                        ${p.local_net('3')} (solder_mask_margin 0.2))
+
+                )
+
+            `
+        }
+    })
+
+    ergogen.inject('footprint', 'anchor_test', {
+        params: {
+            designator: 'T',
+            side: 'F',
+            end: {type: 'anchor', value: undefined}
+        },
+        body: p => {
+            return ` 
+
+                (module anchor_test (layer ${p.side}.Cu) (tedit 5CF31DEF)
+
+                    ${p.at /* parametric position */}
+
+                    (fp_line (start 0 0) (end ${p.end.x} ${p.end.y}) (layer Dwgs.User) (width 0.05))
+
+                )
+
+            `
+        }
+    })
+
+    ergogen.inject('footprint', 'arrobj_test', {
+        params: {
+            designator: 'T',
+            side: 'F',
+            start: {x: 0, y: 0},
+            end: [[1, 0], [0, 1]]
+        },
+        body: p => {
+            lines = ''
+            for (const item of p.end) {
+                lines += `(fp_line (start ${p.start.x} ${p.start.y}) (end ${item[0]} ${item[1]}) (layer Dwgs.User) (width 0.05))\n`
+            }
+            return ` 
+
+                (module arrobj_test (layer ${p.side}.Cu) (tedit 5CF31DEF)
+
+                    ${p.at /* parametric position */}
+
+                    ${lines}
+
+                )
+
+            `
+        }
+    })
+
+    ergogen.inject('references_test', {
+        params: {},
+        body: p => {
+            return `references ${p.ref_hide ? 'hidden' : 'shown'}`
+        }
+    })
+
+    ergogen.inject('template', 'template_test', {
+        convert_outline: () => {},
+        body: params => {
+            return `Custom template override. The secret is ${params.custom.secret}.`
+        }
+    })
+}
